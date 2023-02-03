1939c88cec2129fa4aed61a01f618585b6d0066f
margdaly <margaret.daly@gmail.com>
2023-02-02 19:09:37 -0700

    Add require and three exists tests

---
+++ spec/node_spec.rb
@@ -0,0 +1,28 @@
+require 'rspec'
+require '.lib/node'
+
+RSpec.describe Node do
+  describe 'exists' do
+    it 'is an instance of Node' do
+
+      node = Node.new("plop")
+      
+      expect(node).to be_instnace_of(Node)
+    end
+
+    it 'has data'do
+
+      node = Node.new("plop")
+      
+      expect(node.data).to eq("plop")
+    end
+
+    it 'has a next node' do
+      
+      node = Node.new("plop")
+
+      expect(node.next_node).to eq(nil)  
+    end
+  end
+end
+
