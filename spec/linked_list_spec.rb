8300e1197613c0051187022b9be73f756f1e5e9f
margdaly <margaret.daly@gmail.com>
2023-02-03 18:35:40 -0700

    Add more tests

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require './lib/linked_list'
+require './lib/node'
 
 RSpec.describe LinkedList do
   describe 'exists' do
@@ -19,39 +20,45 @@ RSpec.describe LinkedList do
   end
 
   describe 'can do things' do
-    xit 'can add data' do
+    it 'can add data' do
 
       list = LinkedList.new
 
       expect(list.append("doop")).to eq ("doop")
+      expect(list.head).to be_instance_of(Node)
     end
   end
 
   describe 'knows things' do
-    xit 'knows self' do
+    it 'knows self' do
 
       list = LinkedList.new
       list.append("doop")
-
+# require 'pry'; binding.pry
       expect(list.head.next_node).to eq(nil)
     end
   end
 
   describe 'can do things' do
-    xit 'can count' do
+    it 'can count' do
 
       list = LinkedList.new
       list.append("doop")
 
       expect(list.count).to eq (1)
+      expect(list.head.data).to eq("doop")
+      expect(list.head.next_node).to eq(nil)
+      expect(list.to_string).to eq ("doop")
     end
 
     xit 'can list' do
 
       list = LinkedList.new
       list.append("doop")
+      list.append("deep")
 
-      expect(list.to_string).to eq ("doop")
+      expect(list.head.next_node).to eq(Node.new("deep"))
+      expect(list.to_string).to eq ("doop deep")
     end
   end
 end
