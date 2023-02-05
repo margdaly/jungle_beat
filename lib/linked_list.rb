eadf4ee101a9306a49a17e46b59fb2f992a2d436
margdaly <margaret.daly@gmail.com>
2023-02-05 13:28:56 -0700

    Add unless statement to append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,19 +1,26 @@
 class LinkedList
-  attr_reader :head, :count
+  attr_accessor :head, :count, :current_node
 
   def initialize
     @head = head
     @count = 0
+    @current_node = current_node
+    
   end
 
   def append(data)
-    node = Node.new(data)
+    @head = Node.new(data)
+    unless @count == 0
+      @head.next_node = Node.new(data)
+      @current_node = @head.next_node
+    end
     @count += 1
-    @head = node 
-    node.data
+    data
   end
 
   def to_string
-    @head.data
+    to_string = ""
+    to_string << @head.data 
+    to_string
   end
 end
